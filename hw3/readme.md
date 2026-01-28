0bfe730c4e91ea3782857557c0ab75ee6572935e
Fotato <andrew.callahan@umconnect.umt.edu>
2026-01-27 20:35:25 -0700

    finished hw3

---
+++ hw3/readme.md
@@ -0,0 +1,22 @@
+
+* What you created
+
+I created a simple html page that utilizes HTML, a little bit of CSS and JavaScript. It creates a few logs in the console. My page also has some fun functionality in regards to the smiley face.
+
+* What the generative AI/someone else created
+
+I used Gemini to help me figure out how to actualize my smiley face functionality. I also used it to explain and give me resources so I could understand why the code worked the way it does. Overall, I'm happy with the result and the learning that I acquired during the process.
+
+I attached my own, non-ai generated, comments to the .js for future reference.
+
+* How would you feel if you relied only on AI-generated output or someone else's work
+
+I would learn nothing. It is important to use it to help break down the barriers of learning, but it is also important to study the code (and web resources like w3schools) so that you can grow as an individual.
+
+* Whether you could adequately explain the code that was written
+
+I can and I have. Check the `script.js` file for my comments.
+
+* Whether the final work still feels like your own
+
+I won't lie, but using AI feels like cheating. I look forward to using it to learn, but I can't wait for the day that I won't need it.
---
+++ hw3/script.js
@@ -0,0 +1,76 @@
+//initial console message with things I like!
+console.log("https://www.bay12games.com/dwarves/ https://www.youtube.com/ https://isitchristmas.com/");
+console.log("Dwarf Fortress, Foxhole, Factorio");
+console.log("Frank Frazetta: known for the modern Conan aesthetic, Norman Rockwell: known for realistic slice of life oil paintings, Jerry Gretzinger: created Jerry's Map");
+
+//upon initialization ask the following question
+var universal_answer = prompt("What is the answer to the Ultimate Question of Life, the Universe, and Everything?");
+
+//validate the answer
+if(universal_answer == 42){
+    window.alert("Correct. What does it mean though?")
+} else {
+    window.alert("That can't be right.")
+};
+
+const emotional_face = document.getElementById("emotional_face");
+const message1 = document.getElementById("message1");
+
+const smile = "imgs/smile_500x500.png";
+const surprise = "imgs/surprise_500x500.png";
+const mad = "imgs/mad_500x500.png";
+
+//variable that tracks if the picture has been clicked
+var clicked = false;
+
+/*
+hover interactivity
+
+emotional_face == element in html
+addEventListener == a thing that is constantly listening for something to happen
+mouseover == if the mouse is hovering over the element
+()=> Arrow Function aka run a function, in this case a simple if statement so long as the eventlistener is activated. empty parenthesis are inputs
+or parameters that take arguments. in this case i've passed it the event argument and used it to print to the console. it is not required and can be empty.
+
+it is important to note that the reason why the function exists instead of {} with code in them is because the webpage would execute the code the moment it is read.
+this is the difference between defining a function and immediately calling it
+*/
+emotional_face.addEventListener("mouseover",(e) =>{
+    //if NOT clicked
+    if(!clicked){
+        console.log(e)
+        //use .src for images
+        emotional_face.src = surprise;
+        //use textContent for text
+        message1.textContent = "DON'T CLICK ME!";
+    }
+});
+
+//non-hover interactivity
+// i've used function instead of an arrow function. this is another way to accomplish the same result as the arrow function.
+emotional_face.addEventListener("mouseleave",function(){
+    //if NOT clicked
+    if(!clicked){
+        emotional_face.src = smile;
+        message1.textContent = "don't click me";
+    }
+});
+
+//click interactivity
+
+//this is yet another way to accomplish the same result as the above two eventlisteners
+//here i've defined the function to be run when the image is clicked
+function click_func(){
+    // flips a switch so to speak and freezes the face to mad.
+    clicked = true;
+    //if clicked
+    if(clicked = true){
+        emotional_face.src = mad;
+        message1.textContent = "I TOLD YOU NOT TO DO IT >:("
+    }
+};
+
+//it is important to note that i have purposefully excluded the parenthesis because if i included it the function would immediately execute upon load
+//click_func is a reference to the function
+//click_func() is the function and will be executed, think about calling the function just by itself, click_func(), it makes sense that it would execute immediately.
+emotional_face.addEventListener("click",click_func);
