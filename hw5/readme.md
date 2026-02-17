a66ed267828e8a48fbc39a6a28e1947fd9f60804
Fotato <andrew.callahan@umconnect.umt.edu>
2026-02-16 23:38:17 -0700

    added homework 5 and fixed homework 4 a little

---
+++ hw5/readme.md
@@ -0,0 +1,6 @@
+# AI Section:
+_**Disclaimer**: I am talking about the secretary section in my story. To get there choose the following options: "Call your job and pretend to be sick" -> "Answer the door"_
+
+I used Google Gemini to help me with my input section. It proved to be incredibly useful since I was building something very specific. While I did not copy the code, I did use it to help with syntax. Gemini usually answers my questions with a few different syntax options which allows me a degree of flexibility. I also benefitted from having to describe my code, since I did not want to input my code into Google, which I believe helped me learn.
+
+Overall, my usage of AI was useful, but it offered really complex alternatives to how I was coding, which was welcome in some instances and less helpful in others. I will say that Gemini has made it abundantly clear that some of my coding techniques are a bit inefficient. This is good to know since I am always trying to improve.
--- hw5/script.js
+++ hw5/script.js
@@ -1,17 +1,19 @@
 // associate constant variables to elements
 const header1 = document.getElementById("header1");
 const message_display = document.getElementById("message_display");
-const message = document.getElementById("message");
 const option_display = document.getElementById("option_display");
+const message = document.getElementById("message");
+const sub_message = document.getElementById("sub_message");
 
-// create a generic prompt paragraph element
-const prompt_text = document.createElement("p");
-prompt_text.textContent = "What do you do next?";
-prompt_text.className = "p_prompt";
+// create an button to restart the game
+const restart_button = document.createElement("button");
+restart_button.textContent = "RESTART";
+restart_button.onclick = morning_phase;
 
 function initial_message(){
+    sub_message.textContent = "No pressure."
     header1.textContent = "Welcome" + " " + "To Your Life!";
-    message.textContent = "It's time to live your life! Screw fatalism you're the one in control. Select from the options below to enable your character to live the best possible life. No pressure.";
+    message.textContent = "It's time to live your life! Screw fatalism you're the one in control. Select from the options below to enable your character to live the best possible life.";
     
     // create an button for option select
     const option1 = document.createElement("button");
@@ -24,10 +26,11 @@ function initial_message(){
 function morning_phase(){
     // clear option list
     option_display.innerHTML="";
+    // populate the sub message text
+    sub_message.textContent = "What do you do next?";
 
     header1.textContent = "The Sun Rises...";
     message.textContent = "Your dreams are sweet and pillow-soft, but you are suddenly awoken by an incessant alarm.";
-    message_display.appendChild(prompt_text);
 
     // create buttons for option select
     const option1 = document.createElement("button");
@@ -100,48 +103,141 @@ function afternoon_phase(previous_choice){
 function night_phase(previous_choice){
     // clear option list
     option_display.innerHTML="";
-    //header1.textContent = "Night Has Come At Last...";
 
     if (previous_choice == 1){
         header1.textContent = "The Walk Of Shame...";
         message.textContent = "It is well past noon when you finally arrive at work. Everyone is busy with their dull day-to-day. Your boss calls you into their office. He is angry and he yells at you for a half-hour. Afterward, you leave his office disheveled. At least you weren't fired. You sit down at your desk and check your emails. It seems your luck has changed! In your inbox is a email from an affluent african prince--you stand to gain millions. Your boss is a chump, he'll see."
-        prompt_text.textContent = "$ $ $ Cha-Ching $ $ $";
+        sub_message.textContent = "$ $ $ Cha-Ching $ $ $";
+        option_display.appendChild(restart_button);
+
     } else if (previous_choice == 2){
         header1.textContent = "What A Strange Dream...";
         message.textContent = "You dream about your boss chasing you down a long hallway. You try your best to sprint, but your legs are like lead. The moment before he catches you, you wake with a start. Sweat beads your brow. It was only a dream! Thank God! Suddenly, a floorboard creeks and you look up. Looming in the shrouded corner of your room stands an imposing man in three-piece suit. You do not recognize this man, but you have no doubt as to your boss's involvement. The imposing man cracks his knuckles.";
-        prompt_text.textContent = "You Probably should've gone to work.";
+        sub_message.textContent = "You probably should've gone to work.";
+        option_display.appendChild(restart_button);
+
     } else if (previous_choice == 3){
         header1.textContent = "Future, Past, Present...";
         message.textContent = "You return one of the missed calls thinking it to be your boss or his secretary. Strangely neither picks up and instead an old man answers. He speaks between fits of laughter, \"I haven't much, time, but I'm, you from the future! I know, this won't, make sense right now, but don't, laugh, at Shane's joke!\" Before you can answer he hangs up."
-        prompt_text.textContent = "What could it mean?";
-    
+        sub_message.textContent = "What could it mean?";
+        option_display.appendChild(restart_button);
+
     } else if (previous_choice == 4){
         header1.textContent = "Hilarity To Horror...";
         message.textContent = "You laugh at Shane's joke. It was just what you needed. In the world of routine and constant déjà vu Shane's stupid jokes keep you alive. Of course the joke wasn't that funny, but you can't stop laughing. Oh no! You can't stop laughing! Tears start streaming down your cheeks; the jubilation you once felt turns to panic. This intensifies the laughter until Shane takes a step back. He says something, but you can't hear it over your bellowing laughter."
-        prompt_text.textContent = "\"It wasn\'t that funny\" -Shane"
+        sub_message.textContent = "\"It wasn\'t that funny\" -Shane";
+        option_display.appendChild(restart_button);
+
     } else if (previous_choice == 5){
         header1.textContent = "Hilarity To Hostility...";
         message.textContent = "You don't laugh. Shane stands there, the curtail of his joke flaccid in the stifling silence. You stand there as well, inexpressive, vacant, hollow, rude. What the heck is wrong with you? Shane is a nice guy! \"What the heck is wrong with you!?\" yells Shane. Overwhelmed by the injustice of his joke's reception, he lunges forward and grabs you. Adrenalin courses through his body as he picks you up with a roar. You're terrified. You plead with him, but he cannot hear you from beyond the red veil."
-        prompt_text.textContent = "This will hurt.";
+        sub_message.textContent = "This will hurt.";
+        option_display.appendChild(restart_button);
+
     } else if (previous_choice == 6){
         header1.textContent = "Hilarity To Promotion...";
         message.textContent = "You cut Shane's joke short and begin a new, better joke. At its conclusion he looks skeptically at you: unimpressed. Thankfully your boss heard it and pushes Shane aside. Roaring with laughter he beams down at you like how your father did when you were younger. He finally sees you; he's proud of you at long last."
-        prompt_text.textContent = "Papa?";
+        sub_message.textContent = "Papa?";
+        option_display.appendChild(restart_button);
 
+    // Special Section that uses text input boxes.
     } else if (previous_choice == 7){
         header1.textContent = "Barefaced Liar...";
-        message.textContent = "You answer the door. Your boss's secretary looks at you inquisitively. You try to muster another oscar-worthy act, but in your lethargy and bed-rottenness you can only manage something akin to indifference. The jig is up. She hands you two gatorade bottles and turns on her heel. You watch her as she walks away, her hair swishing austerely."
-        prompt_text.textContent = "You should probably fix up your résumé.";
+        // clear the previous message
+        message.textContent ="";
+
+        //message.textContent = "You answer the door. Your boss's secretary looks at you inquisitively. Choose your next words carefully.";
+        message.insertAdjacentHTML("beforeend", "You open your front door. Your boss's secretary stands before you; limned harshly by daylight. Hey Sara! ");
+        // correct answer is "how"
+        message.insertAdjacentHTML("beforeend", "<input id=\"word1\" type=\"text\" placeholder=\"...\" style=\"width:3ch;\" maxlength=\"3\"> are you? ");
+        // correct answer is "sorry" and "sick" or "bad" or "ill"
+        message.insertAdjacentHTML("beforeend", "<input id=\"word2\" type=\"text\" placeholder=\".....\" style=\"width:5ch;\" maxlength=\"5\"> that I couldn't make it to the office today, I've been feeling really <input id=\"word3\" type=\"text\" placeholder=\"....\" style=\"width:4ch;\" maxlength=\"4\">.");
+        message.insertAdjacentHTML("beforeend","<i>*cough* *cough*</i> ");
+        // correct answer is "tomorrow"
+        message.insertAdjacentHTML("beforeend","I'll probably be at work <input id=\"word4\" type=\"text\" placeholder=\"........\" style=\"width:8ch;\" maxlength=\"8\">, but we'll have to wait and see.")
+        sub_message.textContent = "Here goes nothing...";
+
+        // add a new submission button
+        const submit_button = document.createElement("button");
+        submit_button.textContent = "Submit";
+        submit_button.onclick = secret_night_phase;
+        // add the newly created button to the html element
+        option_display.appendChild(submit_button);
+
     } else if (previous_choice == 8){
         header1.textContent = "Open Sesame...";
         message.textContent = "You pretend to be asleep. A minute later the knocking stops. Then an insidious sensation creeps up your spine. Through half-shut lids you see the secretary approach your bedroom window. Shockingly, she jimmies the window open and crawls through. You almost jump up and shout at her, but then you'd be found out. So instead you lay still and wait. She creeps forward and sets two bottles of gatorade, red flavor, on your nightstand. She sits at the edge of the bed and begins to hum a lullaby. You are not soothed and you suppress shivers of fear."
-        prompt_text.textContent = "Mommy?";
+        sub_message.textContent = "Mommy?";
+        option_display.appendChild(restart_button);
+
     } else{
         header1.textContent = "Not Enough REM...";
         message.textContent = "A day filled with napping turns quickly to panic as you rush to your bedroom window. You throw it open and plunge through. Unfortunately in your bleariness you forgot that you didn't live in an apartment complex and that your house doesn't have a fire escape. Instead of a great escape you tumble through the hedge planted below the window. In the commotion of snapping branches and exclamation of pain your boss's secretary runs up to the bush, \"oh my God! Are you okay?!\"";
-        prompt_text.textContent = "Hope you can muster another oscar-worthy performance.";
+        sub_message.textContent = "Hope you can muster another oscar-worthy performance.";
+        option_display.appendChild(restart_button);
+    }
+}
+
+function secret_night_phase(){
+    // clear option list
+    option_display.innerHTML="";
+    
+    // Each field has an ID and these constant variables map to each element value. Also make all text lower case.
+    let w1 = document.getElementById("word1").value.toLowerCase();
+    let w2 = document.getElementById("word2").value.toLowerCase();
+    let w3 = document.getElementById("word3").value.toLowerCase();
+    let w4 = document.getElementById("word4").value.toLowerCase();
+    console.log(w1);
+    console.log(w2);
+    console.log(w3);
+    console.log(w4);
+    
+    // Are the values correct?
+    if (secret_night_phase_validate(w1,w2,w3,w4)){
+        //success
+        header1.textContent = "Silver Tongue...";
+        message.textContent = "Impressed with your \"candor\" Sara gives you a smile and two red Gatorade bottles. \"I hope so! The office isn't the same without you~\" You try to say something, but thankfully Sara mistakes your stammering for the grumblings of sickitude. Sara turns to leave, but not before giving you a wink. Your mouth starts to get dry. Maybe you are getting sick...";
+        //sub_message.textContent = 
+        for (let i = 0; i < 3; i++){
+            setTimeout(()=>{celebration(i)}, i * 1500);
+        }
+
+    } else{
+        //failure
+        header1.textContent = "Tongue Tied...";
+        message.textContent = "You try to muster another oscar-worthy act, but in your lethargy and bed-rottenness you can only manage something akin to indifference. The jig is up. She hands you two Gatorade bottles and turns on her heel. You watch her as she walks away, her hair swishing austerely.";
+        sub_message.textContent = "You should probably fix up your résumé.";
+    }
+    option_display.appendChild(restart_button);
+}
+
+function secret_night_phase_validate(w1, w2, w3, w4){
+    if (w1 == "how" && w2 == "sorry" && (w3 == "sick" || w3 == "bad" || w3 == "ill") && w4 == "tomorrow"){
+        console.log("user input success");
+        return true;
+    } else{
+        console.log("user input failure");
+        return false;
+    }
+}
+
+// DEBUG -- there is a bug where if you quit out before setTimeout is complete, it will continue to change the sub_message.
+function celebration(i){
+    switch(i){
+            case 0:
+                sub_message.textContent = "You da man!";
+                console.log(i)
+                break;
+            case 1:
+                sub_message.textContent = "Yippee!";
+                console.log(i)
+                break;
+            case 2:
+                sub_message.textContent = "Encore!";
+                console.log(i)
+                break;
     }
 }
 
 // MAIN
-initial_message();
+initial_message()
